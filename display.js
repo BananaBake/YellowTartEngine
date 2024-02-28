dbd70f2058231eb06e678350239eced948ebae00
BananaBake <135561643+BananaBake@users.noreply.github.com>
2024-02-29 00:45:07 +0300

    Update display.js
    Test

--- display.js
+++ display.js
@@ -1,23 +1,23 @@
   displayTart(
-    "/Images/cherry.jpeg",
+    "Images/cherry.jpeg",
     "Cherry tart",
     "YUM,YUM cherries are yummy,especially in a tart",
     "1.99"
   );
  displayTart(
-    "/Images/apple.jpeg",
+    "Images/apple.jpeg",
     "Apple tart",
     "Crispy,flakey crust,filled with apple and cinnamon.How delightful?",
     "1.99"
   );
 displayTart(
-    "/Images/blueberry.jpeg",
+    "Images/blueberry.jpeg",
     "Blueberry tart",
     "Feeling blue? Have one of these to cheer you up!",
     "1.99"
   );
  displayTart(
-    "/Images/pumkin.jpeg",
+    "Images/pumkin.jpeg",
     "Pumkin & Almond tart",
     "Hot sweet,Yummy pumkin in a tart.A   spoon of crème and perfect",
     "1.99"
